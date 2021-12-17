b36fc503f6e184963e0314947324e2f57f50548f
Stetson Done <scdone@gmail.com>
2021-12-17 07:40:13 -0700

    part 2 completed

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Thank you, your form has been submitted!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function duckCompliment(event) {
+	event.preventDefault()
+	alert(`You're someone's reason to smile.`)
+}
+
+let ducky = document.querySelector('img');
+	ducky.addEventListener('mouseover',duckCompliment)
